b7db4ee7fac938a6cd708268bf2753ec0ef28d78
Alberto Enrique Mendoza Pan <ing.amepa@gmail.com>
2019-03-21 02:16:02 -0600

    This is an alredy working version for the usb_test.py

--- usb_test.py
+++ usb_test.py
@@ -1,5 +1,12 @@
 import usb.core
+import usb.util
 import usb.backend.libusb1
 
 #backend = usb.backend.libusb1.get_backend(find_library=lambda x: "C:/Users/Alberto Enrique/Google Drive/Freelancer Projects/Hugo Project/libusb-1.0.dll")
-device = usb.core.find()
+device = usb.core.find(find_all=True)
+
+for dev in device:
+    try:
+        print(dev)
+    except NotImplementedError:
+        print("There is an error")
