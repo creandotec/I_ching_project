3a8cfcebd2062fbe23ead20fad2d5fd1a896be83
Alberto Enrique Mendoza Pan <ing.amepa@gmail.com>
2019-03-22 02:32:53 -0600

    This version find the available usb and check if there is a Bee module

--- usb_test.py
+++ usb_test.py
@@ -2,11 +2,27 @@ import usb.core
 import usb.util
 import usb.backend.libusb1
 
+class BeeModules():
+    idVendor = "0x04d8"
+    modules_available = list()
+
+
+def send_data_to_relay_bee(bee_module, data1, data2):
+    data = [4,data1,data2]
+    bee_module.write(2, data)
+
 #backend = usb.backend.libusb1.get_backend(find_library=lambda x: "C:/Users/Alberto Enrique/Google Drive/Freelancer Projects/Hugo Project/libusb-1.0.dll")
 device = usb.core.find(find_all=True)
 
 for dev in device:
     try:
-        print(dev)
+        #print(hex(dev.idDevice)+"\n")
+        print("USB device, product id {0}, vendor id {1}".format( hex(dev.idProduct), hex(dev.idVendor) ) )
+        #print(hex(dev.idProduct)+"\n")
+        #print(hex(dev.idVendor)+"\n")
+        if hex(dev.idVendor) == BeeModules.idVendor:
+            print("Board found")
+            BeeModules.modules_available.append(dev)
     except NotImplementedError:
         print("There is an error")
+print(len(BeeModules.modules_available))
