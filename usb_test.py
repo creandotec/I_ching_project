efb57cce03c1e46f7dc00b4dafe7822c097c701f
Alberto Enrique Mendoza Pan <ing.amepa@gmail.com>
2019-03-25 00:37:11 -0600

    Working version

--- usb_test.py
+++ usb_test.py
@@ -8,7 +8,7 @@ class BeeModules():
 
 
 def send_data_to_relay_bee(bee_module, data1, data2):
-    data = [4,data1,data2]
+    data = [4, data1, data2]
     bee_module.write(2, data)
 
 #backend = usb.backend.libusb1.get_backend(find_library=lambda x: "C:/Users/Alberto Enrique/Google Drive/Freelancer Projects/Hugo Project/libusb-1.0.dll")
@@ -27,10 +27,15 @@ for dev in device:
             except:
                 pass
             dev.set_configuration()
+            data = [4, 170, 170]
+            dev.write(2, data)
             BeeModules.modules_available.append(dev)
     except NotImplementedError:
         print("There is an error")
 print(len(BeeModules.modules_available))
+
+for dev in BeeModules.modules_available:
+    send_data_to_relay_bee(dev, 170, 170)
 
 while True:
     if len(BeeModules.modules_available) > 0:
@@ -40,4 +45,4 @@ while True:
         for dev in BeeModules.modules_available:
             send_data_to_relay_bee(dev, int(value1), int(value2))
     else:
-        pass
+        break
